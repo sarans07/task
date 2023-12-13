f3bb74d3ddb32bf04908dc0e016cfc65c43dcb49
Indhumathi <Indhumathi@elevadosoftwares.com>
2023-12-13 17:22:47 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:saran_task/saran.dart';
 import 'package:saran_task/task%201.dart';
 import 'package:saran_task/task1.dart';
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: task()
+      home: saran()
     );
   }
 }
---
+++ lib/saran.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class saran extends StatefulWidget {
+  const saran({super.key});
+
+  @override
+  State<saran> createState() => _saranState();
+}
+
+class _saranState extends State<saran> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
